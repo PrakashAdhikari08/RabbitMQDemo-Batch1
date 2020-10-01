f3aa49ba34b0faf2cbb1bc91f097bbd9da47ada2
Prakash <prakashadhikarijhapali@gmail.com>
2020-10-01 21:39:23 +1000

    Project Created for Rabbit MQ Demo

---
+++ src/main/java/com/batch1/rabbitmq/demo/RabbitMqDemoApplication.java
@@ -0,0 +1,13 @@
+package com.batch1.rabbitmq.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/batch1/rabbitmq/demo/RabbitMqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch1.rabbitmq.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
